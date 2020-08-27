a10c8c0e1baf462e65f2ae148f933d3cb749f6c9
Thapelo Tsotetsi <me@tsotetsithapelo.co.za>
2020-08-27 13:12:21 +0200

    Update heroku deployment link.

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ connectDB();
 // init MiddleWare
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the contact keeper API'}));
 
 //Define routes
 app.use('/api/users/', require('./routes/users'));
@@ -22,6 +21,8 @@ if(process.env.NODE_ENV === 'production') {
 // set static server
     app.use(express.static('client/build'));
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+} else {
+    app.get('/', (req, res) => res.json({ msg: 'Welcome to the contact keeper API'}));
 }
 
 const PORT = process.env.PORT || 5000;
